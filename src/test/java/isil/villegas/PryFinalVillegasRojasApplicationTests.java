370418149ace71de718ef0d445255e6d55881f8e
Kento <robinsondotnet@hotmail.com>
2016-07-01 17:38:29 -0500

    first commit

---
+++ src/test/java/isil/villegas/PryFinalVillegasRojasApplicationTests.java
@@ -0,0 +1,18 @@
+package isil.villegas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PryFinalVillegasRojasApplication.class)
+@WebAppConfiguration
+public class PryFinalVillegasRojasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
