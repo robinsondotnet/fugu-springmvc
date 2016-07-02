185a5f3f89edb7b79067d53c42136c3961078425
Alumno-CT <Alumno-CT@C104-08CT>
2016-07-01 21:07:27 -0500

    Recetarios y Recetas Controller are working

---
+++ src/main/java/isil/villegas/controllers/RecetasController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package isil.villegas.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author Alumno-CT
+ */
+@Controller
+public class RecetasController {
+    
+    @RequestMapping("/recetas")
+    public ModelAndView index() {
+        return new ModelAndView("recetas");
+    }
+}
